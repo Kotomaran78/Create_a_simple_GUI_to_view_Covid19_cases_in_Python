fe5ecd9425524b25e0111a507ee1bc43098343f7
Kotomaran78 <66960276+Kotomaran78@users.noreply.github.com>
2021-03-22 18:51:33 +0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.Qt import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import QApplication
+ 
+ 
+ 
+ 
+app = QApplication(sys.argv) 
+web = QWebEngineView()
+web.load(QUrl("http://localhost:8050/"))
+web.show()
+sys.exit(app.exec_())
